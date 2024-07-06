134056112eaae7828dd79e7e4816e1bee3e2c334
harv <harveystephen.guimalan@gmail.com>
2024-07-07 04:16:42 +0800

    added add to database feature

--- routes/work.route.js
+++ routes/work.route.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const db = require("../config/db");
 
+
+//Routes to handle HTTP Requests
 router.get('/', (req, res) => {
-    res.render("index");
+    db.query(`SELECT * FROM Items`, (err, result) => {
+        console.log(result);
+        res.render("index", { item: result});
+    });
+    
+});
+
+router.post('/', (req, res) => {
+    db.query(`INSERT INTO Items (Item) VALUES ("${req.body.item}")`, (err) => {
+        console.log(err);
+    });
+    res.render("index", { item: `:: ${req.body.item} ::` });
 });
 
 
--- server/server.js
+++ server/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const workRouter = require('../routes/work.route');
 
+//define middlewares
+app.use( express.urlencoded( { extended: true } ) );
+
+//define view engine
 app.set("view engine", "ejs");
 
+//define routers
 app.use('/users', workRouter);
 
 app.listen(8081, () => {
