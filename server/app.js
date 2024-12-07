5d8b72b6389574a41146413fdfaf885565d79fb0
harv <harveystephen.guimalan@gmail.com>
2024-12-07 23:10:20 +0800

    added sign up

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
-import EXPRESS from 'express';
-import CORS from 'cors';
-import DB from'./db/dbcon.js';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import EXPRESS from "express";
+import CORS from "cors";
+import DB from "./db/dbcon.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-console.log(__dirname + '/db/dbcon.js');
+console.log(__dirname + "/db/dbcon.js");
 
 const APP = EXPRESS();
 const PORT = 3000;
@@ -29,44 +29,89 @@ APP.use(CORS(), (req, res, next) => {
 DB.query('SHOW TABLES LIKE "users"', (err, result) => {
   if (err) console.log(err);
   if (result.length === 0) {
-    DB.query('CREATE TABLE users ( id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100) NOT NULL, password VARCHAR(100) )', (err) => {
-      if (err) console.log(err);
-      console.log("No Tables yet: Initiating Tables...");
-      console.log("Tables Successfully Initiated...");
-    });
+    DB.query(
+      "CREATE TABLE users ( id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100) NOT NULL, password VARCHAR(100) )",
+      (err) => {
+        if (err) console.log(err);
+        console.log("No Tables yet: Initiating Tables...");
+        console.log("Tables Successfully Initiated...");
+      }
+    );
   } else {
     console.log("Tables Loaded Successfully...");
   }
 });
 
 // HTTP HANDLERS
-APP.post('/api/register', (req, res) => {
+//Get all users and passwords
+APP.post("/api/getUsers", (req, res) => {
+  const changeName = (i) => {
+    name = i;
+  };
+
+  const formData = req.body;
+  let name;
+
+  const queryDB = () => {
+    return new Promise((resolve, reject) => {
+      DB.query("SELECT * FROM users", (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  };
+
+  (async () => {
+    try {
+      const users = await queryDB();
+      for (let i of users) {
+        if (i.name === formData.username && i.password === formData.password) {
+          changeName(i.name);
+          break;
+        }
+      }
+
+      res.status(200).json({
+        username: name,
+        task: 0,
+      });
+    } catch (error) {
+      res.status(500).json({ error: "Database error" });
+    }
+  })();
+});
+
+APP.post("/api/register", (req, res) => {
   const formData = req.body;
   const username = formData.username;
   const fpassword = formData.password;
-  const confirmPassword = formData.confirmPassword
-  if (fpassword === confirmPassword){
+  const confirmPassword = formData.confirmPassword;
+  if (fpassword === confirmPassword) {
     const data = [username, fpassword];
-    DB.query('INSERT INTO users (name, password) VALUES (?, ?)', data, (err, result) => {
-      if (err) console.log(err);
-      console.log(result);
-    });
+    DB.query(
+      "INSERT INTO users (name, password) VALUES (?, ?)",
+      data,
+      (err, result) => {
+        if (err) console.log(err);
+        console.log(result);
+      }
+    );
   }
-  res.status(200).json({ message: 'Registration successful!' });
+  res.status(200).json({ message: "Registration successful!" });
 });
 
-APP.get('/api/is-log', (req, res) => {
+APP.get("/api/is-log", (req, res) => {
   res.status(200).json({
     logStatus: isLoggedIn,
   });
 });
 
-APP.post('/api/log', (req, res) => {
+APP.post("/api/log", (req, res) => {
   const data = req.body;
   changeLog(data.logStatus);
-  res.status(200).json({ message: 'Registration successful!' });
-})
+  res.status(200).json({ message: "Registration successful!" });
+});
 
 APP.listen(PORT, () => {
   console.log("Server is running at PORT: " + PORT);
-})
+});
--- src/components/SignupModal.jsx
+++ src/components/SignupModal.jsx
@@ -1,28 +1,89 @@
-function SignupModal({onCancel, registerHandler}) {
+import $ from "jquery";
 
-  
+function SignupModal({ onCancel, registerHandler }) {
+  async function checkUsers(event) {
+    event.preventDefault();
+    //get form data
+    const formData = $("#form-signup").serializeArray();
+    let data = new Object();
+    data.username = formData[0].value;
+    data.password = formData[1].value;
+    const getUserHeader = new Headers();
+    getUserHeader.append("Content-Type", "application/json");
+    const request = new Request("/api/getUsers", {
+      headers: getUserHeader,
+      mode: "cors",
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+    fetch(request)
+    .then((response) => {
+      if (response.ok) {
+        $('#sign-up').fadeOut(300);
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
 
   return (
     <>
       <div className="absolute w-full h-full flex justify-center items-center ">
-        <form action="/" className="bg-[#D9D9D9] p-4 rounded-lg drop-shadow-xl relative bottom-20 border-[1px] border-black">
+        <form
+          id="form-signup"
+          className="bg-[#D9D9D9] p-4 rounded-lg drop-shadow-xl relative bottom-20 border-[1px] border-black"
+        >
           <p className="text-2xl text-center">Sign Up</p>
           <label htmlFor="username">Username</label>
           <br />
-          <input name="username" type="text" className="border-[#00AAA0] border-[1px] rounded-lg px-2"/>
+          <input
+            name="username"
+            type="text"
+            className="border-[#00AAA0] border-[1px] rounded-lg px-2"
+          />
           <br />
           <label htmlFor="password">Password</label>
           <br />
-          <input name="password" type="password" className="border-[#00AAA0] border-[1px] rounded-lg px-2"/>
+          <input
+            name="password"
+            type="password"
+            className="border-[#00AAA0] border-[1px] rounded-lg px-2"
+          />
           <br />
           <div className="flex justify-around mt-4">
-            <button className="bg-[#B6CFCF] px-2 rounded-full border-[1px] border-black">Submit</button>
-            <button className="bg-[#D55B3E] px-2 rounded-full border-[1px] border-black" onClick={onCancel} type="button">Cancel</button>
+            <button
+              className="bg-[#B6CFCF] px-2 rounded-full border-[1px] border-black"
+              onClick={checkUsers}
+            >
+              Submit
+            </button>
+            <button
+              className="bg-[#D55B3E] px-2 rounded-full border-[1px] border-black"
+              onClick={onCancel}
+              type="button"
+            >
+              Cancel
+            </button>
           </div>
-          <p className="text-sm text-center mt-2 ">Don't have an account yet?</p>
-          <p className="text-center text-blue-500 hover:underline"><button href="#" className="text-sm " type="button" onClick={registerHandler}>Register</button></p>
+          <p className="text-sm text-center mt-2 ">
+            Don't have an account yet?
+          </p>
+          <p className="text-center text-blue-500 hover:underline">
+            <button
+              href="#"
+              className="text-sm "
+              type="button"
+              onClick={registerHandler}
+            >
+              Register
+            </button>
+          </p>
         </form>
-        
       </div>
     </>
   );
